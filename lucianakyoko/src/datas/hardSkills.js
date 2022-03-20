ce83f60dd749407e583b4da7363ab50487d26fe1
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-03-20 15:33:17 -0300

    Create hardSkills datas

---
+++ lucianakyoko/src/datas/hardSkills.js
@@ -0,0 +1,42 @@
+let hardSkills = [
+  {
+    "id": "skill01",
+    "skill": "HTML/ CSS"
+  },
+  {
+    "id": "skill02",
+    "skill": "JavaScript"
+  },
+  {
+    "id": "skill03",
+    "skill": "React"
+  },
+  {
+    "id": "skill04",
+    "skill": "Sass"
+  },
+  {
+    "id": "skill05",
+    "skill": "Figma"
+  },
+  {
+    "id": "skill06",
+    "skill": "Git"
+  },
+  {
+    "id": "skill07",
+    "skill": "GitHub"
+  },
+  {
+    "id": "skill08",
+    "skill": "Scrum/ Kanban"
+  },
+  {
+    "id": "skill09",
+    "skill": "BEM - Methodology"
+  }
+]
+
+export const getHardSkills = () =>{
+  return hardSkills;
+} 
