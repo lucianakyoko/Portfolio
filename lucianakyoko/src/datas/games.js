6e5f6bc8b786f628aa003a50b1956f73981cde82
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-03-20 16:40:33 -0300

    Create games datas

---
+++ lucianakyoko/src/datas/games.js
@@ -0,0 +1,27 @@
+let games = [
+  {
+    "id": "game01",
+    "title": "SuperTrunfo",
+    "description": "Jogo de cartas inspirado no Super Trunfo da Grow. Possui duas modalidades de jogo.",
+    "image": "game01.jpg",
+    "link": "https://supertrunfo-cdz.netlify.app/"
+  },
+  {
+    "id": "game02",
+    "title": "Heroway Game",
+    "description": "O objetivo deste jogo é alcançar o baú de tesouro evitando os monstros e as armadilhas espalhadas no tabuleiro.",
+    "image": "game02.jpg",
+    "link": "https://heroway-game.netlify.app"
+  },
+  {
+    "id": "game03",
+    "title": "Freeway",
+    "description": "Ajude a vaquinha a atravessar a rua. Jogo baseado no Freeway, lançado para o Atari 2600 no ano de 1981.",
+    "image": "game03.jpg",
+    "link": "https://freeway-game.netlify.app/"
+  }
+]
+
+export const getGames = () =>{
+  return games;
+}
