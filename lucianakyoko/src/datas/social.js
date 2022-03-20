ba6d3aa6a735c23d892a1d744d885c493a9e3c1c
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-03-20 15:27:09 -0300

    Create social datas

---
+++ lucianakyoko/src/datas/social.js
@@ -0,0 +1,36 @@
+let social = [
+  {
+    "id":"link01",
+    "site": "Goodreads",
+    "link": "https://www.goodreads.com/user/show/121454267-luciana-kyoko",
+    "icon": "fab fa-goodreads-g"
+  },
+  {
+    "id":"link02",
+    "site": "LinkedIn",
+    "link": "https://www.linkedin.com/in/lucianakyoko",
+    "icon": "fab fa-linkedin-in"
+  },
+  {
+    "id":"link03",
+    "site": "GitHub",
+    "link": "https://github.com/lucianakyoko",
+    "icon": "fab fa-github"
+  },
+  {
+    "id":"link04",
+    "site": "Codepen",
+    "link": "https://codepen.io/lucianakyoko",
+    "icon": "fab fa-codepen"
+  },
+  {
+    "id":"link05",
+    "site": "Stackoverflow",
+    "link": "https://pt.stackoverflow.com/users/213097/luciana-kyoko",
+    "icon": "fab fa-stack-overflow"
+  }
+];
+
+export const getSocial = () => {
+  return social;
+}
