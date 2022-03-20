e28db74453c703c16115c3d7949b0853cbfd63a0
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-03-20 15:17:51 -0300

    Create pages datas

---
+++ lucianakyoko/src/datas/pages.js
@@ -0,0 +1,36 @@
+let pages = [
+  {
+    "id": "page01",
+    "title": "Luciana Kyoko",
+    "link": "/badge"
+  },
+  {
+    "id": "page02",
+    "title": "Portfolio",
+    "link": "/portfolio"
+  },
+  {
+    "id": "page03",
+    "title": "Jogos",
+    "link": "/jogos"
+  },
+  {
+    "id": "page04",
+    "title": "Bootcamps",
+    "link": "/bootcamps"
+  },
+  {
+    "id": "page05",
+    "title": "Sobre",
+    "link": "/sobre"
+  },
+  {
+    "id": "page06",
+    "title": "Contato",
+    "link": "/contato"
+  }
+];
+
+export const getPages = () => {
+  return pages;
+}
