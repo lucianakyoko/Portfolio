e6a2aac9237965aaf223836fecb3054c441fcc86
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-03-20 16:19:56 -0300

    Create Pulse component

---
+++ lucianakyoko/src/components/Pulse/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Pulse = () => {
+  return (
+    <div className='pulse'>
+      <div className='pulse__dot'></div>
+    </div>
+  );
+};
+
+export default Pulse;
